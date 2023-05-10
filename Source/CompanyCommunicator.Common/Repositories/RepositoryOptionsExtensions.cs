35d9c315acf246a4903c11956b93270d8e847c8b
Alysson Nishiyama <castortroynz@gmail.com>
2023-05-10 14:47:02 -0300

    Suporte para tabela e blob em storages separados

---
+++ Source/CompanyCommunicator.Common/Repositories/RepositoryOptionsExtensions.cs
@@ -0,0 +1,29 @@
+// <copyright file="RepositoryOptionsExtensions.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// </copyright>
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories
+{
+    public static class RepositoryOptionsExtensions
+    {
+        public static IServiceCollection AddRepositoryOptions(this IServiceCollection services, bool isItExpectedThatTableAlreadyExists = true)
+        {
+            services.AddOptions<RepositoryOptions>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                settings.StorageAccountConnectionString = configuration.GetValue<string>("StorageAccountConnectionString");
+                settings.BlobStorageAccountConnectionString = configuration.GetValue<string>("BlobStorageAccountConnectionString");
+                if (string.IsNullOrWhiteSpace(settings.BlobStorageAccountConnectionString))
+                {
+                    settings.BlobStorageAccountConnectionString = settings.StorageAccountConnectionString;
+                }
+                settings.EnsureTableExists = !configuration.GetValue<bool>("IsItExpectedThatTableAlreadyExists", isItExpectedThatTableAlreadyExists);
+            });
+            return services;
+        }
+    }
+}
